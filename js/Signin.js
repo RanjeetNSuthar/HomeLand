65d501a3c5b435f2d602132253922e8fe17a3305
Mrugesh07 <mrugesh.hnde@gmail.com>
2021-10-26 12:13:48 +0530

    popup form and about us changed

---
+++ js/Signin.js
@@ -0,0 +1,9 @@
+
+function openSigninForm() {
+    document.getElementById("signinForm").style.display = "block";
+  }
+  
+  function closeSigninForm() {
+    document.getElementById("signinForm").style.display = "none";
+  }
+  
---
+++ js/Signup.js
@@ -0,0 +1,8 @@
+
+function openSignupForm() {
+  document.getElementById("signupForm").style.display = "block";
+}
+
+function closeSignupForm() {
+  document.getElementById("signupForm").style.display = "none";
+}
